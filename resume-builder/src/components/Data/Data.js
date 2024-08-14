0e89a72c87e2fba4be4d584e8cf2df61d38c2d16
Rajnish kumar <100439674+Rajnishkr8541@users.noreply.github.com>
2024-08-14 17:31:37 +0530

    Resume-Builder
    A dynamic web application designed to simplify the process of creating professional resumes. Users can fill in their personal details, customize templates, and generate polished resumes in real-time. Built with React, Node.js, Express, and MongoDB, this full-stack application ensures a seamless user experience, with secure authentication, intuitive form inputs, and various template options for resume creation. Ideal for job seekers looking to create customized resumes quickly and efficiently.
    
    Features:
    
    User authentication and protected routes
    Dynamic form for easy input of personal and professional details
    Multiple resume templates to choose from
    Real-time resume preview
    Mobile-responsive design for ease of use across devices
    Tech Stack: React, Node.js, Express, MongoDB, JWT, Bootstrap

---
+++ resume-builder/src/components/Data/Data.js
@@ -0,0 +1,45 @@
+export const templateImagesPaths =[
+    //this object contains the paths of the static template images that will be used to display template images in the Home page.
+    {
+        
+        imageSource:require('./images/temp1.jpg'),
+    },
+    {
+        
+        imageSource:require('./images/temp2.jpg'),
+    },
+    {
+        
+        imageSource:require('./images/temp3.jpg'),
+    },
+    {
+        
+        imageSource:require('./images/temp4.jpg'),
+    },
+    {
+       
+        imageSource:require('./images/temp5.jpg'),
+    },
+    {
+        
+        imageSource:require('./images/temp6.jpg'),
+    },
+    {
+        
+        imageSource:require('./images/temp7.jpg'),
+    },
+    {
+        
+        imageSource:require('./images/temp8.jpg'),
+    },
+    // {
+    //     name:'Template 9',
+    //     imageSource:require('./images/temp9.jpg'),
+    // }
+]
+
+export const stateNames =['Andhra Pradesh',"Andaman and Nicobar Islands","Arunachal Pradesh","Assam","Bihar","Chandigarh"
+,"Chhattisgarh" ,"Dadar and Nagar Haveli","Daman and Diu","Delhi","Lakshadweep","Puducherry","Goa","Gujarat","Haryana"
+,"Himachal Pradesh","Jammu and Kashmir","Jharkhand","Karnataka","Kerala","Madhya Pradesh","Maharashtra","Manipur","Meghalaya"
+,"Mizoram","Nagaland","Odisha","Punjab","Rajasthan","Sikkim","Tamil Nadu","Telangana","Tripura","Uttar Pradesh","Uttarakhand","West Bengal"
+]
